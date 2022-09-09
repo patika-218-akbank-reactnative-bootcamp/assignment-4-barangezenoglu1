4f567e6a848a427574d0b6278069eed4f2d5b7de
barangezen <b.gezenoglu@emakina.com.tr>
2022-09-10 01:25:44 +0300

    Theme feature is added, theme data stored in redux state

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View, Dimensions} from 'react-native';
+import {useSelector} from 'react-redux';
 import WelcomeLogo from '../assets/WelcomeLogo.png';
 
 const windowWidth = Dimensions.get('window').width;
 
 export const Movie = ({title, voteAverage, posterPath}) => {
+  const themeColors = useSelector(state => state.theme);
+  const styles = StyleSheet.create({
+    container: {
+      position: 'relative',
+      backgroundColor: '#D6D0D6',
+      width: windowWidth / 2 - 15,
+      marginRight: 10,
+      marginTop: 10,
+      borderRadius: 10,
+      height: 400,
+    },
+    posterContainer: {
+      alignItems: 'center',
+    },
+    poster: {
+      marginTop: 10,
+      width: windowWidth / 2 - 40,
+      height: 300,
+      borderRadius: 10,
+    },
+    textContainer: {
+      marginTop: 10,
+      marginLeft: 15,
+    },
+    text: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: themeColors.theme.black,
+    },
+    avarageVoteContainer: {
+      marginTop: 10,
+      marginLeft: 15,
+      marginBottom: 10,
+    },
+    avarageVote: {
+      fontSize: 15,
+      fontWeight: '700',
+      color: themeColors.theme.black,
+    },
+  });
+
   return (
     <View style={title ? styles.container : ''}>
       <View style={styles.posterContainer}>
@@ -28,40 +70,3 @@ export const Movie = ({title, voteAverage, posterPath}) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    position: 'relative',
-    backgroundColor: '#D6D0D6',
-    width: windowWidth / 2 - 15,
-    marginRight: 10,
-    marginTop: 10,
-    borderRadius: 10,
-  },
-  posterContainer: {
-    alignItems: 'center',
-  },
-  poster: {
-    marginTop: 10,
-    width: windowWidth / 2 - 40,
-    height: 300,
-    borderRadius: 10,
-  },
-  textContainer: {
-    marginTop: 10,
-    marginLeft: 15,
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  avarageVoteContainer: {
-    marginTop: 10,
-    marginLeft: 15,
-    marginBottom: 10,
-  },
-  avarageVote: {
-    fontSize: 15,
-    fontWeight: '700',
-  },
-});
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,9 +1,22 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {useSelector} from 'react-redux';
 import {MoviesList} from '../components/MoviesList';
 
 export const Search = ({navigation}) => {
   const [searchInput, setSearchInput] = useState('');
+  const themeColors = useSelector(state => state.theme);
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: themeColors.theme.white,
+    },
+    searchMovie: {
+      marginTop: 20,
+      height: 50,
+      backgroundColor: 'rgba(55, 54, 54, 0.5)',
+    },
+  });
   return (
     <View style={styles.container}>
       <TextInput
@@ -21,14 +34,3 @@ export const Search = ({navigation}) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  searchMovie: {
-    marginTop: 20,
-    height: 50,
-    backgroundColor: 'rgba(55, 54, 54, 0.5)',
-  },
-});
